f8badd79692c7a6d6412ef1ec99a949f774060e3
Sykander <Sykander.Gul@rockar.com>
2019-03-27 13:19:01 +0000

    Node - Setup - Restructured app.js file to be more OOP

--- app.js
+++ app.js
@@ -2,24 +2,54 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// DB init
 const Database = require('./db');
-const db = new Database();
-db.connect();
-db.seed();
-
-// App init
-const app = express();
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
-// Get Routes
 const Router = require('./Router');
-const router = new Router();
-router.useControllers();
 
-// Add Routes
-app.use('/api', router.router);
-app.listen(process.env.PORT || 8080);
+class App 
+{
+    constructor() {
+        this.app = new express();
+        this.app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        this.app.use(bodyParser.json());
+    }
+    
+    /**
+     * Run the App
+     */
+    run() {
+        this.getDb();
+        this.getRoutes();
+        this.addRoutes();
+        console.log('App is running.');
+    }
+    
+    /**
+     * Get Database connection and seed
+     */
+    getDb() {
+        this.db = new Database();
+        this.db.connect();
+        this.db.seed();
+    }
+    
+    /**
+     * Get Routes 
+     */
+    getRoutes() {
+        this.router = new Router();
+        this.router.useControllers();
+    }
+    
+    /**
+     * Add Routes to App with Middleware
+     */
+    addRoutes() {
+        this.app.use('/api', this.router.router);
+        this.app.listen(process.env.PORT);
+    }
+}
+
+const app = new App();
+app.run();
